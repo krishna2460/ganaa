1eb4164697259583c540933d7bf7692ad7aceb12
krishna2460 <krishnaagrawal2460@gmail.com>
2024-09-03 21:59:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:5500/songs/`)
+    let a = await fetch(`http://127.0.0.1:5501/songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -110,7 +110,7 @@ async function displayAlbums() {
 
             //get the meta data
             
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch(`http://127.0.0.1:5501/songs/${folder}/info.json`)
             let response = await a.json();
             // console.log(response)
             cardContainer.innerHTML = cardContainer.innerHTML + `  <div  data-folder="${folder}"  class="card">
